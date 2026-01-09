64a8d2cb69ea4bb670e0c694a186c04c0e38601a
iorpa <ifratorpa32@gmail.com>
2026-01-09 17:33:44 +0600

    Add login and registration pages

--- config.php
+++ config.php
@@ -6,7 +6,6 @@ $password = "";
 $dbname = "syndicatebuster";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
-
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
