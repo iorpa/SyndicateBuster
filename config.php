5fdaca6a7d0ef0153cbefc96e18ea9d6df7d6cfc
iorpa <ifratorpa32@gmail.com>
2026-01-15 21:40:26 +0600

    updated login signup page

--- config.php
+++ config.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 session_start();
 $servername = "localhost";
@@ -14,20 +13,3 @@ else {
     echo "Connection successful";
 }
 ?>
-=======
-<?php
-session_start();
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "syndicatebuster";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-else {
-    echo "Connection successful";
-}
-?>
->>>>>>> 09f6418f665c4c9e3f3b2a742eea527adc036c3f
